51119c46aeb124890c5c1b26afbfd5f81ea6dffb
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-03 17:44:33 +0200

    Add project description

--- University system excercise/Student.cpp
+++ University system excercise/Student.cpp
@@ -1,3 +1,16 @@
+/* 
+    You have been hired as a system developer at a newly created ZCAS University.
+    As part of your first assignment, you are tasked to develop a system to manage the
+    operations of this University. You are told that the departments that need to be considered as part
+    of the pilot project include the Admission, Accounts and Dean of students respectively.
+    The admission department wants the system to be able to admit the students after capturing their names,
+    nrc, address, results, student number and gender. On the other hand, the account department is concerned
+    with the tuition fees, user fees, accommodation fees, transport, name and the number of student.
+    The dean of students is concerned with the system allowing him to capture the student’s number,
+    names, hostel name and room number. Implement this ZCAS University system by making use of objects and classes
+    as learned in class. Follow the guidelines of object oriented programming as learnt in class and other requirement
+    is free style.
+*/
 
 #include <iostream>
 using namespace std;
